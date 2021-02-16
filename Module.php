47b060dee1e9063cc8d9d14a99c5fe624e0c86f8
Jon Goodger <jonno.is@gmail.com>
2021-02-16 12:27:58 +0000

    Apply WME updates

--- Module.php
+++ Module.php
@@ -5,14 +5,22 @@ class Module
 {
     public function onBootstrap($e)
     {
-        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
+        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
             $controller      = $e->getTarget();
             $controllerClass = get_class($controller);
             $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
             $config          = $e->getApplication()->getServiceManager()->get('config');
-            if (isset($config['module_layouts'][$moduleNamespace])) {
-                $controller->layout($config['module_layouts'][$moduleNamespace]);
+
+            $routeMatch = $e->getRouteMatch();
+            $actionName = strtolower($routeMatch->getParam('action', 'not-found')); // get the action name
+
+            if (isset($config['module_layouts'][$moduleNamespace][$actionName])) {
+                $controller->layout($config['module_layouts'][$moduleNamespace][$actionName]);
+            }elseif(isset($config['module_layouts'][$moduleNamespace]['default'])) {
+                $controller->layout($config['module_layouts'][$moduleNamespace]['default']);
             }
+
+
         }, 100);
     }
 }
